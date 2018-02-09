86892a343f90bab4fed0b65d3623b7bb9d42b740
Pedro Henrique Schneider <8975487+pedrohesch@users.noreply.github.com>
2018-02-09 09:51:24 -0200

    utils py

---
+++ ml_utils.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+import matplotlib.pyplot as plt
+import itertools
+import numpy as np
+
+def plot_confusion_matrix(cm, classes, normalize=False, title='Confusion matrix', cmap=plt.cm.Accent):
+    """
+    This function prints and plots the confusion matrtix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print("Not Normalized confusion matrix")
+
+    print(cm)
+
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, cm[i, j],
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+
+
+def plot_ROC_curve(y_true, y_score):
+    fpr, tpr, threshold = metrics.roc_curve(y_true, y_score)
+    roc_auc = metrics.auc(fpr, tpr)
+    plt.figure()
+    plt.plot(fpr, tpr, color='darkorange', lw=2, label='ROC curve (area = %0.2f)' % roc_auc)
+    plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title(u'Característica de Operação do Receptor (ROC)')
+    plt.legend(loc="lower right")
+    plt.show()
